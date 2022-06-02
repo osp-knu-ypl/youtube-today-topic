4420c72f90719a9819d6fc8ff0a9e21a295bd4b8
gunwoo <miniapple212@gmail.com>
2022-06-02 09:06:04 +0000

    crawl + processing

--- data_processing/crawler.py
+++ data_processing/crawler.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 import requests
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import lxml
+import analysis
 
 url = "https://www.youtube.com/feed/trending"
 session = HTMLSession()
@@ -14,14 +16,13 @@ data2 = page.html.find('yt-formatted-string#description-text.style-scope.ytd-vid
 all_data = []
 for i in data:
     test = list()
-    test.append(i.attrs["title"])
+    title = i.attrs["title"]
     test.append(i.attrs["href"])
+    p_title = analysis.filter(title)
+    test.append(p_title)
     all_data.append(test)
 
 j = 0
 for i in data2 :
-    all_data[j].append(i.text)
+    all_data[j].append(analysis.filter(i.text))
     j += 1
-
-for i in all_data:
-    print(i)
