2c5bc80abd0bd099e2fb70eda02c447994cfe525
SeokHyeonLee <dltjr0703@gmail.com>
2022-06-02 18:27:13 +0900

    change topic on html and flask

--- app.py
+++ app.py
@@ -10,41 +10,11 @@ def index():
 def method():
     return "method"
 
-@app.route('/apple/', methods=['GET', 'POST'])
+@app.route('/trend/', methods=['GET', 'POST'])
 def apple():
-    return render_template('/Phone/apple.html')
+    return render_template('/Trending/trend.html')
 
-@app.route('/samsung/', methods=['GET', 'POST'])
-def samsung():
-    return render_template('/Phone/samsung.html')
 
-@app.route('/tablet/', methods=['GET', 'POST'])
-def tablet():
-    return render_template('/Phone/tablet.html')
-
-@app.route('/cpu/', methods=['GET', 'POST'])
-def cpu():
-    return render_template('/PC/cpu.html')
-
-@app.route('/gpu/', methods=['GET', 'POST'])
-def gpu():
-    return render_template('/PC/gpu.html')
-
-@app.route('/ssd/', methods=['GET', 'POST'])
-def ssd():
-    return render_template('/PC/ssd.html')
-
-@app.route('/tv/', methods=['GET', 'POST'])
-def tv():
-    return render_template('/ETC/tv.html')
-
-@app.route('/watch/', methods=['GET', 'POST'])
-def watch():
-    return render_template('/ETC/watch.html')
-
-@app.route('/console/', methods=['GET', 'POST'])
-def console():
-    return render_template('/ETC/console.html')
 
 @app.route('/creators/', methods=['GET', 'POST'])
 def creators():
--- data_processing/Content_Analysis.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def data_processing(content, price, link):
-    processed_data = {}
-    processed_data["ID"] = random.randrange(1, 100000000000)
-    processed_data["Classification"] = content.split()
-    processed_data["Price"] = price
-    processed_data["Link"] = link
-    return processed_data
