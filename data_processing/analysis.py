3ce688963dafd8467ca3afa6ef2363251e41fdbd
You dong hoon <edwinuo3@gmail.com>
2022-06-06 09:42:38 +0000

    mod auto.sh and analysis.py

--- data_processing/analysis.py
+++ data_processing/analysis.py
@@ -7,26 +7,30 @@ from konlpy.tag import Twitter
 def hfilter(s):
     return re.sub(u'[^ \.\,\?\!\u3130-\u318f\uac00-\ud7a3]+','',s)
 
+def titlefilter(s):
+        return re.sub(u'[\"\']+','',string)
+
 def otherhfilter(s):
     return re.sub('[^ a-zA-Z]','',s).strip()
 
-def filter(str):
-    
-    word = []
-    posTagger = Twitter()
-    p_entry = hfilter(str)
-    p_entry2 = posTagger.pos(p_entry)
+def filter(str, code):
 
-    for i in p_entry2:
-        if i[1] == "Noun":
-            word.append(i[0])
-    
-    return word
+        if(code == "KR"):
+            word = []
+            posTagger = Twitter()
+            p_entry = hfilter(str)
+            p_entry2 = posTagger.pos(p_entry)
+
+            for i in p_entry2:
+                if i[1] == "Noun":                                                                  word.append(i[0])     
+            return word                                    
+        else:
+            otherfilter(str,code)
 
 def otherfilter(str, code):
 
     word = []
-    nlp = spacy.load(icode)
+    nlp = spacy.load(code)
     p_entry = otherhfilter(str)
     doc = nlp(str)
     p_entry2 = list(doc)
