cfefa40f9d6a3e719f61fe812e27e95f84e21bcf
You dong hoon <edwinuo3@gmail.com>
2022-06-06 06:33:25 +0000

    shell update

--- auto.sh
+++ auto.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H)(h)"
+#echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H)(h)"
+vardate=" $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H)(h)"
+echo $vardate
+
+python3 ./data_processing/crawler.py KR
+python3 ./data_processing/crawler.py US
+
