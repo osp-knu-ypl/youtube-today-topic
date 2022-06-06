fd99746e22e7167b931dddd01c62c0f93927ddc8
You dong hoon <edwinuo3@gmail.com>
2022-06-06 09:44:37 +0000

    mod auto

--- auto.sh
+++ auto.sh
@@ -4,6 +4,6 @@
 vardate=" $(date +%Y)$(date +%m)$(date +%d)$(date +%H)"
 echo $vardate
 
-python3 ./data_processing/crawler.py KR $vardate ko_core_news_sm
+python3 ./data_processing/crawler.py KR $vardate KR
 #python3 ./data_processing/crawler.py US $vardate en_core_web_sm
 
