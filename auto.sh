95e950c8863e5176070094eb83735b28a8c9e9ed
You dong hoon <edwinuo3@gmail.com>
2022-06-09 11:34:15 +0000

    shell automation

--- auto.sh
+++ auto.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H)(h)"
-vardate="$(date +%Y)$(date +%m)$(date +%d)$(date +%H)"
-echo $vardate
+while :
+do
+	# echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H)(h)"
+	vardate="$(date +%Y)$(date +%m)$(date +%d)$(date +%H)"
+	echo $vardate
 
-# support language pack countrycode / time / language pack
+	# support language pack countrycode / time / language pack
 
-python3 ./data_processing/crawler.py KR $vardate KR
-#python3 ./data_processing/crawler.py US $vardate en_core_web_sm
+	python3 ./data_processing/crawler.py KR $vardate KR
+	#python3 ./data_processing/crawler.py US $vardate en_core_web_sm
+	#python3 ./data_processing/crawler.py ES $vardate es_core_news_sm
+	#python3 ./data_processing/crawler.py FR $vardate fr_core_news_sm
+	#python3 ./data_processing/crawler.py DE $vardate de_core_news_sm
+	#python3 ./data_processing/crawler.py RU $vardate ru_core_news_sm
+	#python3 ./data_processing/crawler.py IT $vardate it_core_news_sm
+	#python3 ./data_processing/crawler.py GR $vardate el_core_news_sm
+	#python3 ./data_processing/crawler.py CN $vardate zh_core_news_sm
 
+	sleep 43200
+done
